7a1973ee95928e64ed86bc67cc9cf4762b2b816c
JBryskiewicz <105501793+JBryskiewicz@users.noreply.github.com>
2025-10-17 15:10:12 +0200

    init

---
+++ src/main/java/pl/vizjal1/messanger_app_demo/MessangerDemoBeApplication.java
@@ -0,0 +1,13 @@
+package pl.vizjal1.messanger_app_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessangerDemoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessangerDemoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/vizjal1/messanger_app_demo/MessangerDemoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.vizjal1.messanger_app_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessangerDemoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
